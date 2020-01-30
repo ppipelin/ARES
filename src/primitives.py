ac76cff4a13f601d298d777d999e8bbea55b2c3d
Ronan Cailleau <ro.cailleau@gmail.com>
2020-01-30 15:47:43 +0100

    Added colors to the render of the cube

--- src/primitives.py
+++ src/primitives.py
@@ -2,7 +2,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-verticies = (
+verticies = [
     (1, -1, -1),
     (1, 1, -1),
     (-1, 1, -1),
@@ -10,28 +10,35 @@ verticies = (
     (1, -1, 1),
     (1, 1, 1),
     (-1, -1, 1),
-    (-1, 1, 1)
-    )
+    (-1, 1, 1),
+]
 
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+colors = [
+    (255.0, 0.0, 0.0),
+    (0.0, 255.0, 0.0),
+    (0.0, 0.0, 255.0),
+]
+
+edges = [
+    (0, 1, 0),
+    (0, 3, 1),
+    (0, 4, 2),
+    (2, 1, 0),
+    (2, 3, 1),
+    (2, 7, 2),
+    (6, 3, 0),
+    (6, 4, 1),
+    (6, 7, 2),
+    (5, 1, 0),
+    (5, 4, 1),
+    (5, 7, 2),
+]
 
 
 def Cube():
     glBegin(GL_LINES)
     for edge in edges:
-        for vertex in edge:
+        glColor(colors[edge[2]][0], colors[edge[2]][1], colors[edge[2]][2], 255.0)
+        for vertex in (edge[0], edge[1]):
             glVertex3fv(verticies[vertex])
     glEnd()
